1960bb1d3ec8eb7f5b2b66f88beca5847a0c72f9
yor_email <daniil.stebnitsky@gmail.com>
2022-11-13 14:14:24 +0300

    5. ResponseEntity [#504799]

--- src/main/java/ru/job4j/auth/controller/PersonController.java
+++ src/main/java/ru/job4j/auth/controller/PersonController.java
@@ -5,6 +5,7 @@ import org.postgresql.util.PSQLException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +15,8 @@ import ru.job4j.auth.service.PersonService;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
@@ -61,27 +64,63 @@ public class PersonController {
         if (result) {
             persons.deleteById(id);
         }
-        return ResponseEntity.ok().body(result);
+        return ResponseEntity
+                .status(result ? HttpStatus.OK : HttpStatus.NOT_FOUND)
+                .header("message", result ? "person successfully deleted"
+                        : String.format("person with id %d not exists", id))
+                .body(result);
     }
 
     @PostMapping("/sign-up")
-    public boolean signUp(@RequestBody Person person) throws PSQLException {
+    public ResponseEntity<Boolean> signUp(@RequestBody Person person) throws PSQLException {
         String password = person.getPassword();
         if (password.length() < 6) {
             throw new IllegalArgumentException("Password should be more than six symbols.");
         }
         person.setPassword(encoder.encode(person.getPassword()));
-        return persons.save(person).isPresent();
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(persons.save(person).isPresent());
+    }
+
+    @GetMapping("/photo/{id}")
+    public ResponseEntity<byte[]> showPhoto(@PathVariable int id) throws IOException {
+        var person = persons.findById(id);
+        var response = ResponseEntity
+                .status(HttpStatus.NOT_FOUND).body(new byte[0]);
+        if (person.isPresent()) {
+            var content = Files.readAllBytes(Path.of(person.get().getPhoto()));
+            response = ResponseEntity.ok()
+                    .contentType(MediaType.IMAGE_JPEG)
+                    .contentType(MediaType.IMAGE_PNG)
+                    .contentType(MediaType.IMAGE_GIF)
+                    .contentLength(content.length)
+                    .body(content);
+        }
+        return response;
     }
 
     @ExceptionHandler(value = { IllegalArgumentException.class })
     public void exceptionHandler(Exception e,
                                  HttpServletRequest request,
                                  HttpServletResponse response) throws IOException {
+        handleError(e, request, response, e.getMessage());
+    }
+
+    @ExceptionHandler(value = { IOException.class })
+    public void ioExceptionHandler(Exception e,
+                                 HttpServletRequest request,
+                                 HttpServletResponse response) throws IOException {
+        handleError(e, request, response, "file not found");
+    }
+
+    private void handleError(Exception e, HttpServletRequest request,
+                             HttpServletResponse response, String message) throws IOException {
         response.setStatus(HttpStatus.BAD_REQUEST.value());
         response.setContentType("application/json");
         response.getWriter().write(objectMapper.writeValueAsString(new HashMap<>() { {
-            put("message", e.getMessage());
+            put("request", request.getRequestURI());
+            put("message", message);
             put("type", e.getClass());
         }}));
         LOGGER.error(e.getLocalizedMessage());
